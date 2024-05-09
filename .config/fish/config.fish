d3a59439b03aed8b4bb79b2ba0e3ed3a48007ecd
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2024-05-09 17:29:10 +0900

    Update

---
+++ .config/fish/config.fish
@@ -0,0 +1,4 @@
+set -x PATH $PATH $HOME/.mozbuild/git-cinnabar
+set -x PATH $PATH $HOME/.cargo/bin
+set -x PATH $PATH $HOME/.local/bin
+set -x PATH $PATH $HOME/.mozbuild/android-sdk-linux/platform-tools
